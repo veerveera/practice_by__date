10d87a47ba76dd9c2cfa30ffd3263454e83db4f3
veerveera <78911323+veerveera@users.noreply.github.com>
2024-03-05 15:02:16 +0300

    Update 28_2.cpp

--- 28_2.cpp
+++ 28_2.cpp
@@ -5,7 +5,49 @@ int rank;
 int N, i, k, z, z1, z2;
 int sum = 0;
 
+void first() {
+#pragma omp for
+	for (int i = 0; i < 4; i++)
+		std::cout << "hello world!" << std::endl;
+	return;
+};
+
+void second() {
+int k;
+#pragma omp parallel for
+		std::cout << "Enter number" << std::endl;
+		std::cin >> k;
+		for (int i = 0; i < k; i++)
+			std::cout << "I am " << i << " thread from " << k << " threads!" << std::endl;
+	return;
+}
+
+void thrid() {
+	int k, rank;
+	std::cout << "Enter count of threads: " << std::endl;
+	std::cin >> k;
+
+#pragma omp parallel num_threads(k)
+	{
+#pragma omp critical
+		{
+			rank = omp_get_thread_num();
+			std::cout << "I'm " << rank << " thread." << std::endl;
+		}
+	}
+}
+//fourth(last)
+
 int main() {
+
+	first();
+	std::cout << "------------------------" << std::endl;
+	second();
+	std::cout << "------------------------" << std::endl;
+	thrid();
+	std::cout << "------------------------" << std::endl;
+
+
 	std::cout << "Enter number N: " << std::endl;
 	std::cin >> N;
 	std::cout << "Enter number of threads k :" << std::endl;
