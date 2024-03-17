7bfb4e1964d16db9510ce76974de1fcbd0c0cfb9
veerveera <78911323+veerveera@users.noreply.github.com>
2024-03-17 12:31:20 +0300

    Update 12_03.cpp

--- 12_03.cpp
+++ 12_03.cpp
@@ -7,8 +7,8 @@
 using namespace std;
 
 int main() {
-
-	cv::Mat image = cv::imread("C:/Users/veras/Desktop/emae.jpg");
+	cv::Mat image = cv::imread("C:/Users/veras/Desktop/piy.jpeg");
+	cv::Mat image1 = cv::imread("C:/Users/veras/Desktop/piy.jpeg");
 	if (image.empty()) {
 		std::cout << "Error image" << std::endl;
 		return -1;
@@ -45,7 +45,36 @@ int main() {
 
 //second
 
+   cv::Mat GRAYImage;
+   cvtColor(image, GRAYImage, cv::COLOR_BGR2GRAY);
+   cv::imshow("GRAY", GRAYImage);
 
-	cv::waitKey(0);
-	return 0;
+   cv::Mat gauss;
+   cv::GaussianBlur(GRAYImage, gauss, cv::Size(5, 5), 0);
+
+   std::vector<cv::Vec4i> lines;
+
+   cv::Mat edges;
+   cv::Canny(GRAYImage, edges, 70, 300);
+   cv::HoughLinesP(edges, lines, 1, CV_PI / 180, 100, 1, 20);
+
+
+   for (size_t i = 0; i < lines.size(); i++) {
+	   cv::Vec4i l = lines[i];
+	   cv::line(image1, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(255, 255, 0), 3, cv::LINE_AA);
+   }
+
+   std::vector<cv::Vec3f> circles;
+
+   cv::HoughCircles(gauss, circles, cv::HOUGH_GRADIENT, 1, GRAYImage.rows / 15, 55, 40, 0, 0);
+
+   for (size_t i = 0; i < circles.size(); i++) {
+	   cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+	   int radius = cvRound(circles[i][2]);
+	   cv::circle(image1, center, radius, cv::Scalar(0, 0, 255), 2, 8, 0);
+   }
+
+   cv::imshow("Result", image1);
+   cv::waitKey(0);
+   return 0;
 }
